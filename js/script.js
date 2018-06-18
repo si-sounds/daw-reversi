69b8dc03e32f2677a4439fc570d6d85381ac4f16
si-sounds <si_07sounds@yahoo.co.jp>
2018-06-18 17:24:17 +0900

    0618

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ const n = "pic/dummy.png";	//no disc
 const k = "pic/kuro.png";	//kuro
 const s = "pic/siro.png";	//shiro
 
+var turn = k;
+
 var disc = [];
 for(var i = 1; i < 9; i++) {
 	disc[i] = [];
@@ -9,7 +11,7 @@ for(var i = 1; i < 9; i++) {
 		disc[i][j] = document.createElement("img");
 		disc[i][j].id = "d" + i + j;
 		disc[i][j].src = n;
-		disc[i][j].onclick = "change();";
+		disc[i][j].addEventListener("click", change(i,j));
 		disc[i][j].style = "top:" + ((i-1)*5+0.2) + "em; left:" + ((j-1)*5+0.2) + "em;";
 		document.getElementById("disc").appendChild(disc[i][j]);
 	}
@@ -20,36 +22,35 @@ disc[4][5].src = k;
 disc[5][4].src = k;
 disc[5][5].src = s;
 
-function change(x, y){
-	var a = x;
-	var b = y;
 
-	do{
-		a = a+1;
-		if(a>7){break;}
-	}while(disc[1][1].src != disc[1][1].src);
-	if(a<8){
+
+function change(x, y){
+	return function(){
+		console.log(x,y);
+		var a = x;
+		var b = y;
 		do{
-			a = a-1;
-			if(true){
-				changeK(a, b);
-			}else{
-				changeS(a, b);
-			}
-		}while(a == x);
+			a = a+1;
+			if(a>8){break;}
+		}while(disc[a][b].src != disc[x][y].src);
+		if(a<8){
+			do{
+				a = a-1;
+				if(turn == k){
+					changeK(a, b);
+				}else{
+					changeS(a, b);
+				}
+			}while(a == x);
+		}
 	}
+
 }
 
 function changeK(x, y){
-	disc[1][1].src = k;
+	disc[x][y].src = k;
 }
 
 function changeS(x, y){
-	disc[1][1].src = s;
+	disc[x][y].src = s;
 }
-
-
-
-
-
-
